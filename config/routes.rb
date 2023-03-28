0902b72784851b6c23bedf0c4afa7f43924585d0
Kevin Yang <e510021@gmail.com>
2023-03-28 16:46:35 +0800

    refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root "pages#home"
-
   get "/resumes", to: "resumes#index"
   get "resumes/new", to: "resumes#new", as: "new_resume"
   get "/resumes/:id", to: "resumes#show", as: "resume"
