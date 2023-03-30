2ea1a1531c910bbf6e155dc4256bf0651c47d5b0
Kevin Yang <e510021@gmail.com>
2023-03-30 11:30:55 +0800

    adding user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   root "pages#home"
-  get "/resumes", to: "resumes#index"
-  get "resumes/new", to: "resumes#new", as: "new_resume"
-  get "/resumes/:id", to: "resumes#show", as: "resume"
-  get "/resumes/:id/edit", to: "resumes#edit", as: "edit_resume"
-  patch "/resumes/:id", to: "resumes#update"
-  delete "resumes/:id", to: "resumes#destroy"
-  post "/resumes", to: "resumes#create"
+
+  resource :user, except: %i[new destroy] do
+    get "/sign_up", action: "new"
+  end
+  resources :resumes
+
   get "/about", to: "pages#about"
   get "/contact", to: "pages#contact"
 end
